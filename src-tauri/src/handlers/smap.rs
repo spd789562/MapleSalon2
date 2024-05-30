86194a202448d2d941d56048030ddd5dcda17715
spd789562 <leo.yicun.lin@gmail.com>
2024-05-30 23:12:37 +0800

    [edit] fix handler::resolve_smap when content is empty

--- src-tauri/src/handlers/smap.rs
+++ src-tauri/src/handlers/smap.rs
@@ -1,7 +1,7 @@
-use wz_reader::{property, WzNodeArc};
+use wz_reader::{WzNodeArc, WzNodeCast};
 
 use super::path::SMAP_PATH;
-use crate::{Error, Result};
+use crate::Result;
 
 pub fn get_smap(root: &WzNodeArc) -> Result<WzNodeArc> {
     let node = root.read().unwrap();
@@ -17,10 +17,14 @@ pub fn resolve_smap(node: &WzNodeArc) -> Result<Vec<(String, String)>> {
     let mut result = Vec::with_capacity(node.children.len());
 
     for child in node.children.values() {
+        let child_read = child.read().unwrap();
         let name = child.read().unwrap().name.to_string();
-        let text = property::string::resolve_string_from_node(&child)
-            .map_err(|_| Error::NodeTypeMismatch("string"))?;
-        result.push((name, text));
+        if let Some(string) = child_read.try_as_string() {
+            let text = string.get_string()?;
+            result.push((name, text));
+        } else {
+            result.push((name, String::new()));
+        }
     }
 
     Ok(result)
