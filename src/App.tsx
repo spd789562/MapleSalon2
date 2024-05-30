24e3877eed8aab4a540e878e06d514e724eb3200
spd789562 <leo.yicun.lin@gmail.com>
2024-05-30 23:15:45 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { createSignal } from 'solid-js';
+import logo from './assets/logo.svg';
+import { invoke } from '@tauri-apps/api/core';
+import './App.css';
+
+function App() {
+  const [greetMsg, setGreetMsg] = createSignal('');
+  const [name, setName] = createSignal('');
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke('greet', { name: name() }));
+  }
+
+  return (
+    <div class="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <div class="row">
+        <a href="https://solidjs.com" target="_blank" rel="noreferrer">
+          <img src={logo} class="logo solid" alt="Solid logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg()}</p>
+    </div>
+  );
+}
+
+export default App;
