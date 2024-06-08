b1a647b6135f875f905c47ecb9dea52a57a5761e
spd789562 <leo.yicun.lin@gmail.com>
2024-06-08 16:12:59 +0800

    [add] hsvAdjustmentFilter

---
+++ src/renderer/filter/hsvAdjustment.frag
@@ -0,0 +1,81 @@
+in vec2 vTextureCoord;
+out vec4 finalColor;
+
+uniform sampler2D uTexture;
+uniform vec3 uHsl;
+uniform float uColorStart;
+uniform float uColorEnd;
+
+
+/* code from https://gist.github.com/983/e170a24ae8eba2cd174f */
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb(vec3 c) {
+    c = vec3(c.x, clamp(c.yz, 0.0, 1.0));
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+// code from: https://github.com/pixijs/filters/blob/main/src/hsl-adjustment/hsladjustment.frag#L18
+const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+
+vec3 hueShift(vec3 color, float angle) {
+    float cosAngle = cos(angle);
+    return vec3(
+    color * cosAngle +
+      cross(k, color) * sin(angle) +
+      k * dot(k, color) * (1.0 - cosAngle)
+    );
+}
+
+void main() {
+    vec4 color = texture(uTexture, vTextureCoord);
+    
+    vec3 resultRGB = color.rgb;
+
+    float hue = uHsl[0];
+    float saturation = uHsl[1];
+    float value = uHsl[2];
+
+    vec3 tohsv = rgb2hsv(resultRGB);
+    
+    float h = tohsv.x;
+    float s = tohsv.y;
+    float v = tohsv.z;
+
+    // hue
+    resultRGB = hueShift(resultRGB, hue);
+    tohsv = rgb2hsv(resultRGB);
+
+    bool isTransparent = color.a == 0.;
+    bool notEffect = isTransparent || (!isTransparent && tohsv.y <= .1);
+
+    if (h >= uColorStart && h <= uColorEnd) {
+        // value
+        if (value < 0.) {
+          // why multuply by lightness tho?
+          tohsv.z += v * value * tohsv.y;
+        } else if (value > 0.) {
+          tohsv.y *= max(.0, .82 - value);
+          tohsv.z = clamp(tohsv.z * (1. + value * 0.4), 0., 0.99);
+        }
+        // saturation
+        if (saturation > 0.) {
+          tohsv.y += tohsv.y * saturation * 6.;
+        } else if (value > 0.) {
+          tohsv.y += tohsv.y * saturation;
+        }
+        resultRGB = hsv2rgb(tohsv);
+    }
+
+    finalColor = vec4(resultRGB, color.a);
+}
---
+++ src/renderer/filter/hsvAdjustmentFilter.ts
@@ -0,0 +1,162 @@
+import { Filter, GlProgram, GpuProgram } from 'pixi.js';
+import { vertex, wgslVertex } from 'pixi-filters';
+import fragment from './hsvAdjustment.frag';
+// import source from './hsvAdjustment.wgsl';
+
+export enum ColorRange {
+  All = 0,
+  Red = 1,
+  Yellow = 2,
+  Green = 3,
+  Cyan = 4,
+  Blue = 5,
+  Purple = 6,
+}
+
+const MapleColorRange = {
+  [ColorRange.All]: [0, 1],
+  [ColorRange.Red]: [0, 0.1],
+  [ColorRange.Yellow]: [0.1, 0.25],
+  [ColorRange.Green]: [0.25, 0.42],
+  [ColorRange.Cyan]: [0.42, 0.59],
+  [ColorRange.Blue]: [0.59, 0.75],
+  [ColorRange.Purple]: [0.75, 1],
+};
+
+/**
+ * Options for the hsvAdjustmentFilter constructor.
+ */
+export interface hsvAdjustmentFilterOptions {
+  /**
+   * The amount of hue in degrees (-180 to 180)
+   * @default 0
+   */
+  hue: number;
+  /**
+   * The amount of color saturation (-1 to 1)
+   * @default 0
+   */
+  saturation: number;
+  /**
+   * The amount of lightness (-1 to 1)
+   * @default 0
+   */
+  lightness: number;
+  /**
+   * The filter effect range
+   * @default ColorRange.All
+   */
+  colorRange: ColorRange;
+}
+
+/**
+ * ![original](../screenshots/original.png)![filter](../screenshots/hsl-adjustment.png)
+ *
+ * This WebGPU filter has been ported from the WebGL renderer that was originally created by Viktor Persson (@vikpe)
+ *
+ * @class
+ * @extends Filter
+ */
+export class hsvAdjustmentFilter extends Filter {
+  /** Default values for options. */
+  public static readonly DEFAULT_OPTIONS: hsvAdjustmentFilterOptions = {
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+    colorRange: ColorRange.All,
+  };
+
+  public uniforms: {
+    uHsl: Float32Array;
+    uColorStart: number;
+    uColorEnd: number;
+  };
+
+  private _hue!: number;
+
+  private _colorRange: ColorRange = ColorRange.All;
+
+  /**
+   * @param options - Options for the hsvAdjustmentFilter constructor.
+   */
+  constructor(options?: Partial<hsvAdjustmentFilterOptions>) {
+    const localOption = { ...hsvAdjustmentFilter.DEFAULT_OPTIONS, ...options };
+
+    // const gpuProgram = GpuProgram.from({
+    //     vertex: {
+    //         source: wgslVertex,
+    //         entryPoint: 'mainVertex',
+    //     },
+    //     fragment: {
+    //         source,
+    //         entryPoint: 'mainFragment',
+    //     },
+    // });
+
+    const glProgram = GlProgram.from({
+      vertex,
+      fragment,
+      name: 'hsl-adjustment-filter',
+    });
+
+    const range = MapleColorRange[localOption.colorRange];
+
+    super({
+      glProgram,
+      resources: {
+        hslUniforms: {
+          uHsl: { value: new Float32Array(3), type: 'vec3<f32>' },
+          uColorStart: { value: range[0], type: 'f32' },
+          uColorEnd: { value: range[1], type: 'f32' },
+        },
+      },
+    });
+
+    this.uniforms = this.resources.hslUniforms.uniforms;
+    this.hue = localOption.hue;
+  }
+
+  /**
+   * The amount of hue in degrees (-180 to 180)
+   * @default 0
+   */
+  get hue(): number {
+    return this._hue;
+  }
+  set hue(value: number) {
+    this._hue = value;
+    this.resources.hslUniforms.uniforms.uHsl[0] = value * (Math.PI / 180);
+  }
+
+  /**
+   * The amount of lightness (-1 to 1)
+   * @default 0
+   */
+  get saturation(): number {
+    return this.resources.hslUniforms.uniforms.uHsl[1];
+  }
+  set saturation(value: number) {
+    this.resources.hslUniforms.uniforms.uHsl[1] = value;
+  }
+
+  /**
+   * The amount of lightness (-1 to 1)
+   * @default 0
+   */
+  get lightness(): number {
+    return this.resources.hslUniforms.uniforms.uHsl[2];
+  }
+  set lightness(value: number) {
+    this.resources.hslUniforms.uniforms.uHsl[2] = value;
+  }
+
+  get colorRange(): ColorRange {
+    return this._colorRange;
+  }
+  set colorRange(value: ColorRange) {
+    this._colorRange = value;
+    const range = MapleColorRange[value];
+    this.resources.hslUniforms.uniforms.uColorStart = range[0];
+    this.resources.hslUniforms.uniforms.uColorEnd = range[1];
+  }
+}
