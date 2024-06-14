6cadb9f0455980c7553b6820fd442939930acabb
spd789562 <leo.yicun.lin@gmail.com>
2024-06-14 19:00:35 +0800

    [add] some ui

---
+++ src/components/ui/ellipsisText.tsx
@@ -0,0 +1,36 @@
+import type { Assign, HTMLArkProps } from '@ark-ui/solid';
+import { mergeProps, splitProps } from 'solid-js';
+import { Dynamic } from 'solid-js/web';
+import { css, cx } from 'styled-system/css';
+import { splitCssProps } from 'styled-system/jsx';
+import { type TextVariantProps, text } from 'styled-system/recipes';
+import type { JsxStyleProps } from 'styled-system/types';
+
+export interface EllipsisTextProps
+  extends Assign<JsxStyleProps, HTMLArkProps<'p'>>,
+    TextVariantProps {
+  as?: 'p' | 'label' | 'div' | 'span';
+  clamp?: number;
+}
+
+export const EllipsisText = (props: EllipsisTextProps) => {
+  const mergedProps = mergeProps({ as: 'p' }, props);
+  const [variantProps, textProps] = splitProps(mergedProps, [
+    'size',
+    'variant',
+  ]);
+  const [cssProps, elementProps] = splitCssProps(textProps);
+  const [localProps, rootProps] = splitProps(elementProps, ['as', 'class']);
+  const className = text(variantProps);
+
+  return (
+    <Dynamic
+      component={localProps.as}
+      class={cx(className, css(cssProps), localProps.class)}
+      whiteSpace="nowrap"
+      overflow="hidden"
+      textOverflow="ellipsis"
+      {...rootProps}
+    />
+  );
+};
---
+++ src/components/ui/rowVirtualizer.tsx
@@ -0,0 +1,75 @@
+import { For, createMemo, onMount, type JSX } from 'solid-js';
+
+import { createVirtualizer } from '@tanstack/solid-virtual';
+
+import { Flex } from 'styled-system/jsx/flex';
+
+export interface RowVirtualizerProps<Item> {
+  columnCount: number;
+  data: Item[];
+  renderItem: (item: Item) => JSX.Element;
+}
+export function RowVirtualizer<Item>(props: RowVirtualizerProps<Item>) {
+  let parentRef!: HTMLDivElement;
+
+  const count = createMemo(() =>
+    Math.ceil(props.data.length / props.columnCount),
+  );
+
+  const columnWidth = createMemo(() => 100 / props.columnCount);
+
+  const timesArray = Array.from({ length: props.columnCount });
+
+  const virtualizer = createVirtualizer({
+    count: count(),
+    getScrollElement: () => parentRef,
+    estimateSize: () => 45,
+  });
+
+  const items = virtualizer.getVirtualItems();
+
+  return (
+    <div ref={parentRef} style={{ height: '100%', overflow: 'auto' }}>
+      <div
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+          width: '100%',
+          position: 'relative',
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            transform: `translateY(${items[0]?.start ?? 0}px)`,
+          }}
+        >
+          <For each={items}>
+            {(virtualRow) => {
+              let ref!: HTMLDivElement;
+              onMount(() => virtualizer.measureElement(ref));
+
+              return (
+                <Flex ref={ref} data-index={virtualRow.index}>
+                  <For each={timesArray}>
+                    {(_, index) => (
+                      <div style={{ width: `${columnWidth()}%` }}>
+                        {props.renderItem(
+                          props.data[
+                            virtualRow.index * props.columnCount + index()
+                          ],
+                        )}
+                      </div>
+                    )}
+                  </For>
+                </Flex>
+              );
+            }}
+          </For>
+        </div>
+      </div>
+    </div>
+  );
+}
