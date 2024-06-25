aa2adc6ebb8a7b11ea262cebaba663c64b072a00
spd789562 <leo.yicun.lin@gmail.com>
2024-06-25 16:59:09 +0800

    [add] color range select and reset button

--- src/components/EquipEdit/EquipHsvAdjust.tsx
+++ src/components/EquipEdit/EquipHsvAdjust.tsx
@@ -8,8 +8,45 @@ import {
 } from '@/store/character';
 import { useDynamicPureStore } from '@/store';
 
+import ResetIcon from 'lucide-solid/icons/rotate-ccw';
 import { VStack } from 'styled-system/jsx/vstack';
+import { HStack } from 'styled-system/jsx/hstack';
+import { SimpleSelect as Select } from '@/components/ui/select';
+import { Button } from '@/components/ui/button';
 import { EquipHsvSlider } from './EquipHsvSlider';
+
+import { ColorRange } from '@/renderer/filter/hsvAdjustmentFilter';
+
+const ColorOtions = [
+  {
+    label: '整體色系',
+    value: ColorRange.All,
+  },
+  {
+    label: '紅色系',
+    value: ColorRange.Red,
+  },
+  {
+    label: '黃色系',
+    value: ColorRange.Yellow,
+  },
+  {
+    label: '綠色系',
+    value: ColorRange.Green,
+  },
+  {
+    label: '青綠色系',
+    value: ColorRange.Cyan,
+  },
+  {
+    label: '藍色系',
+    value: ColorRange.Blue,
+  },
+  {
+    label: '紫色系',
+    value: ColorRange.Purple,
+  },
+];
 
 export interface EquipHsvAdjustProps {
   id: number;
@@ -19,7 +56,8 @@ export const EquipHsvAdjust = (props: EquipHsvAdjustProps) => {
   const itemChange = useDynamicPureStore(getItemChangeById);
 
   const createItemChange =
-    (property: 'hue' | 'saturation' | 'brightness') => (value: number) => {
+    (property: 'colorRange' | 'hue' | 'saturation' | 'brightness') =>
+    (value: number) => {
       const category = itemChange()?.category;
       if (category) {
         $currentItemChanges.setKey(
@@ -29,13 +67,41 @@ export const EquipHsvAdjust = (props: EquipHsvAdjustProps) => {
       }
     };
 
+  function handleResetAll() {
+    const category = itemChange()?.category;
+    if (category) {
+      const subCategory = getCharacterSubCategory(category);
+      $currentItemChanges.setKey(`${subCategory}.colorRange`, 0);
+      $currentItemChanges.setKey(`${subCategory}.hue`, 0);
+      $currentItemChanges.setKey(`${subCategory}.saturation`, 0);
+      $currentItemChanges.setKey(`${subCategory}.brightness`, 0);
+    }
+  }
+  const handleColorRangeChange = createItemChange('colorRange');
   const handleHueChange = createItemChange('hue');
   const handleSaturationChange = createItemChange('saturation');
   const handleBrightnessChange = createItemChange('brightness');
 
   return (
     <Show when={itemChange()}>
-      <VStack>
+      <VStack pt={1}>
+        <HStack w="full" alignItems="center">
+          <Button variant="outline" size="sm" onClick={handleResetAll}>
+            <ResetIcon />
+            重置
+          </Button>
+          <Select
+            width="[40%]"
+            placeholder="染色區間"
+            items={/* @once */ ColorOtions}
+            positioning={{ sameWidth: true }}
+            size="sm"
+            value={[(itemChange()?.item.colorRange || 0) as unknown as string]}
+            onValueChange={(detail) => {
+              handleColorRangeChange(Number(detail.value[0]));
+            }}
+          />
+        </HStack>
         <EquipHsvSlider
           property={/* @once */ 'hue'}
           value={itemChange()?.item.hue || 0}
