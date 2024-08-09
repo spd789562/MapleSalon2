e5b01ed7d0e432af7028c4d28d93f1e165f28676
spd789562 <leo.yicun.lin@gmail.com>
2024-08-09 18:48:59 +0800

    [edit] remove testing items when prod

--- src/store/character/store.ts
+++ src/store/character/store.ts
@@ -32,60 +32,74 @@ export const $currentScene = atom<PreviewScene>(PreviewScene.Color);
 
 export const $sceneCustomColor = atom<string>('#FFFFFF');
 
-export const $currentCharacterItems = deepMap<Partial<CharacterItems>>({
+let DefaultItems: Partial<CharacterItems> = {
   Head: {
     id: 2000,
   },
   Body: {
     id: 12000,
   },
-  Face: {
-    id: 56772,
-    dye: {
-      color: 6,
-      alpha: 50,
+};
+
+if (import.meta.env.DEV) {
+  DefaultItems = {
+    Head: {
+      id: 2000,
     },
-  },
-  Hair: {
-    id: 47046,
-    dye: {
-      color: 0,
-      alpha: 50,
+    Body: {
+      id: 12000,
     },
-  },
-  Cap: {
-    // hat half cover
-    id: 1006105,
-    enableEffect: true,
-  },
-  // Cap: {
-  //   //  hat full cover
-  //   id: 1000003,
-  // },
-  'Face Accessory': {
-    id: 1012764,
-  },
-  'Eye Decoration': {
-    id: 1022285,
-  },
-  Earrings: {
-    id: 1032331,
-    enableEffect: true,
-  },
-  Overall: {
-    id: 1054098,
-  },
-  Shoes: {
-    id: 1073273,
-  },
-  Cape: {
-    id: 1103642,
-    enableEffect: true,
-  },
-  Weapon: {
-    id: 1703369,
-  },
-});
+    Face: {
+      id: 56772,
+      dye: {
+        color: 6,
+        alpha: 50,
+      },
+    },
+    Hair: {
+      id: 47046,
+      dye: {
+        color: 0,
+        alpha: 50,
+      },
+    },
+    Cap: {
+      // hat half cover
+      id: 1006105,
+      enableEffect: true,
+    },
+    // Cap: {
+    //   //  hat full cover
+    //   id: 1000003,
+    // },
+    'Face Accessory': {
+      id: 1012764,
+    },
+    'Eye Decoration': {
+      id: 1022285,
+    },
+    Earrings: {
+      id: 1032331,
+      enableEffect: true,
+    },
+    Overall: {
+      id: 1054098,
+    },
+    Shoes: {
+      id: 1073273,
+    },
+    Cape: {
+      id: 1103642,
+      enableEffect: true,
+    },
+    Weapon: {
+      id: 1703369,
+    },
+  };
+}
+
+export const $currentCharacterItems =
+  deepMap<Partial<CharacterItems>>(DefaultItems);
 
 export const $currentCharacterInfo = map({
   id: 'default',
