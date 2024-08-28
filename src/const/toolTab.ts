b18746a18c29c9cce68a31cd9a2cc4755e997561
spd789562 <leo.yicun.lin@gmail.com>
2024-08-28 19:02:43 +0800

    [add] prepare item dye table related store

--- src/const/toolTab.ts
+++ src/const/toolTab.ts
@@ -2,12 +2,14 @@ export enum ToolTab {
   AllAction = 'allAction',
   HairDye = 'hairDye',
   FaceDye = 'faceDye',
+  ItemDye = 'itemDye',
 }
 
 export const ToolTabNames: Record<ToolTab, string> = {
   [ToolTab.AllAction]: '全部動作',
   [ToolTab.HairDye]: '髮型顏色',
   [ToolTab.FaceDye]: '臉型顏色',
+  [ToolTab.ItemDye]: '裝備染色表',
 };
 
 export enum ActionExportType {
--- src/store/toolTab.ts
+++ src/store/toolTab.ts
@@ -1,7 +1,69 @@
-import { atom } from 'nanostores';
+import { atom, deepMap } from 'nanostores';
 
+import type { EquipSubCategory } from '@/const/equipments';
 import { type ToolTab, ActionExportType } from '@/const/toolTab';
+import { CharacterAction } from '@/const/actions';
 
 export const $toolTab = atom<ToolTab | undefined>(undefined);
 
 export const $actionExportType = atom<ActionExportType>(ActionExportType.Gif);
+
+export const $onlyShowDyeable = atom<boolean>(false);
+export const $preserveOriginalDye = atom<boolean>(false);
+export const $selectedEquipSubCategory = atom<EquipSubCategory[]>([]);
+export const $dyeResultCount = atom<number>(72);
+export const $dyeAction = atom<CharacterAction>(CharacterAction.Stand1);
+
+export enum DyeOrder {
+  Up = 'up',
+  Down = 'down',
+}
+export interface DyeConfigOption {
+  enabled: boolean;
+  order: DyeOrder;
+}
+export type DyeConfig = {
+  hue: DyeConfigOption;
+  saturation: DyeConfigOption;
+  lightness: DyeConfigOption;
+};
+export const $dyeConfig = deepMap({
+  hue: {
+    enabled: false,
+    order: DyeOrder.Up,
+  },
+  saturation: {
+    enabled: false,
+    order: DyeOrder.Up,
+  },
+  lightness: {
+    enabled: false,
+    order: DyeOrder.Up,
+  },
+});
+
+/* actions */
+export function toggleDyeConfigEnabled(key: keyof DyeConfig, value: boolean) {
+  $dyeConfig.setKey(`${key}.enabled`, value);
+}
+export function toggleDyeConfigOrder(key: keyof DyeConfig, order: DyeOrder) {
+  $dyeConfig.setKey(`${key}.order`, order);
+}
+export function selectDyeCategory(category: EquipSubCategory) {
+  const current = $selectedEquipSubCategory.get();
+  if (current.includes(category)) {
+    return;
+  }
+  $selectedEquipSubCategory.set([...$selectedEquipSubCategory.get(), category]);
+}
+export function deselectDyeCategory(category: EquipSubCategory) {
+  $selectedEquipSubCategory.set(
+    $selectedEquipSubCategory.get().filter((c) => c !== category),
+  );
+}
+export function setDyeResultCount(count: number) {
+  $dyeResultCount.set(count);
+}
+export function setDyeAction(action: CharacterAction) {
+  $dyeAction.set(action);
+}
